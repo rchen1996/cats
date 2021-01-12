f1ab5e28bc452230192fe63ca5c2752d33746053
Rebecca Chen <rebecca061996@gmail.com>
2021-01-12 19:16:50 +0000

    example of async to return data

---
+++ asyncBreeds.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    // ISSUE: Returning from inner callback function, not breedDetailsFromFile
+    if (!error) functionToRunWhenThingsAreDone(data);
+  });
+  // ISSUE: Attempting to return data out here will also not work
+  // Currently not returning anything from here - function returns undefined
+};
+
+// CHANGE 1: Moved the console.log into a new function:
+const printOutCatBreed = breed => {
+  console.log('Return Value: ', breed)// print out details correctly
+};
+
+//CHANGE 2: passing 2 arguments into breedDetailsFromFile: breed string and callback function
+const bombay = breedDetailsFromFile("Bombay", printOutCatBreed);
